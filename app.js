8de1efecd83b30078b1d2fbace89835075898c5b
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-04-15 09:10:17 +0300

    Add animation for text

--- app.js
+++ app.js
@@ -11,5 +11,30 @@ navbarBtn.addEventListener("click", () => {
 	const isOpen = navbarLinks.classList.contains("open");
 	navbarBtnIcon.setAttribute("class", isOpen?"ri-close-line":"ri-menu-3-fill");
 
+	//removing .open class and chaging icon menu when you click on menu link
+
+	navbarLinks.addEventListener("click", (e) => {
+		navbarLinks.classList.remove("open");
+		navbarBtnIcon.setAttribute("class", "ri-menu-3-fill")
+	}) 
+
 	//ri-close-line
 })
+
+const scrollRevealOption = {
+	distance: "50px",
+	origin: "bottom",
+	duration: 1000,
+};
+
+ScrollReveal().reveal(".header__content .section__header", {
+	...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__content .section__description", {
+	...scrollRevealOption,
+	delay: 500,
+});
+ScrollReveal().reveal(".header__content .header__btn", {
+	...scrollRevealOption,
+	delay: 1000,
+});
